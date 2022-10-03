54c743b7fd47f7c6be3f233093524e0d56beac34
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2022-10-03 11:32:36 +0530

    Create enable-alarm-actions.py

---
+++ lambda/cloudwatch/enable-alarm-actions.py
@@ -0,0 +1,7 @@
+import boto3
+cloudwatch = boto3.client('cloudwatch')
+
+def lambda_handler(event, context):
+    response = cloudwatch.enable_alarm_actions(
+        AlarmNames=['alarms-names']
+    )
